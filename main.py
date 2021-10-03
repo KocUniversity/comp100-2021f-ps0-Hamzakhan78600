b795df6c37bdeb8686e945806f1e23a52df87b7b
Hamza78600 <91738893+Hamzakhan78600@users.noreply.github.com>
2021-10-03 18:21:46 +0000

    rectified a mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy
-x=int(input("Enter number x : 3"))
-y=int(input("Enter number y : 3"))
-print("x**y =",  x**y)
-print("log(x)=",numpy.log2(x) )
+import math
+x=int(input("Enter number x : "))
+y=int(input("Enter number y : "))
+print("x**y =", x**y)
+print("log(x)=",math.log2(x) )
 print("78946")
