e947b601e6ded614f18a7f950e9f86dceaff32e1
Hamza78600 <91738893+Hamzakhan78600@users.noreply.github.com>
2021-10-03 18:07:24 +0000

    print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-Enter number x: 2
-Enter number y: 3
-x**y = 8
-log(x) = 1
-75308
-'
+import numpy
+x=int(input("Enter number x : 3"))
+y=int(input("Enter number y : 3"))
+print("x**y =",  x**y)
+print("log(x)=",numpy.log2(x) )
+print("78946")
